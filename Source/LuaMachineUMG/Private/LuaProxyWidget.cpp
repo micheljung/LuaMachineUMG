1c80d7601b0b4d0bb7e238d936c7714e9066c8fe
Michel Jung <michel.jung89@gmail.com>
2025-07-12 18:31:48 +0200

    Support text justification

--- Source/LuaMachineUMG/Private/LuaProxyWidget.cpp
+++ Source/LuaMachineUMG/Private/LuaProxyWidget.cpp
@@ -110,6 +110,7 @@ bool ULuaProxyWidget::IsKnownProperty(const FString& Key)
 		"CheckedState",
 		"BrushColor",
 		"Brush",
+		"Justification",
 	};
 
 	return KnownProperties.Contains(FName(Key));
